a25ac6496c24588d90cd7c02222baf9a494167f6
whyut <wahyu.inayanto@gmail.com>
2019-02-07 19:17:48 +0700

    Update scriptbootcache.sh

--- scriptbootcache.sh
+++ scriptbootcache.sh
@@ -3,4 +3,6 @@ sudo mkdir /mnt/resource/localcache
 sudo chown -R nginx.nginx /mnt/resource/localcache
 sudo chmod -R 777 /mnt/resource/localcache
 service firewalld stop
+service iptables stop
+systemctl stop firewalld.service
 csf -x
